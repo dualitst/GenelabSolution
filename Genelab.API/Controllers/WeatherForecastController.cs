aeeca79afae0962af5c7f957ca45f4ff2f04853c
fabian romero baez <fabromerobaez@outlook.es>
2021-01-15 12:27:04 -0600

    Desacoplamiento docker

--- Genelab.API/Controllers/WeatherForecastController.cs
+++ Genelab.API/Controllers/WeatherForecastController.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Genelab.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
         public class WeatherForecastController : ControllerBase
     {
