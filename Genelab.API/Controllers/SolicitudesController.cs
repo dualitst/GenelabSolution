d5698920bca1245844140f5fbf385976d80b438c
fabian romero baez <fabromerobaez@outlook.es>
2021-01-17 07:38:35 -0600

    cambios para rama de prueba

---
+++ Genelab.API/Controllers/SolicitudesController.cs
@@ -0,0 +1,66 @@
+﻿using Genelab.API.Models;
+using Genelab.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Genelab.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SolicitudesController : ControllerBase
+    {
+        /// <summary>
+        /// Devuelve una lista de personas con filtro
+        /// </summary>
+        /// <param name="oParameter">Finsoft.Common.Model.periodos - Filtra por los artributos SET FILTERS HERE</param>
+        /// <returns></returns>
+        [HttpPost("list")]
+        public IActionResult GetList()
+        {
+            try
+            {
+                UserModel userModel1 = new UserModel();
+                UserModel userModel2 = new UserModel();
+                UserModel userModel3 = new UserModel();
+
+                List<UserModel> listUsers = new List<UserModel>();
+
+                userModel1.UserName = "fabian.romero@genelab.com";
+                userModel1.FullName = "Fabian Romero";
+                userModel1.Correo = "fabian.romero@genelab.com";
+                userModel1.Activo = "Si";
+                userModel1.Rol = "Administrador";
+
+                userModel2.UserName = "victor.gonzalez@genelab.com";
+                userModel2.FullName = "Victor Gonzalez";
+                userModel2.Correo = "victor.gonzalez@genelab.com";
+                userModel2.Activo = "Si";
+                userModel2.Rol = "Analista";
+
+                userModel3.UserName = "arturo.gomez@genelab.com";
+                userModel3.FullName = "Arturo Gomez";
+                userModel3.Correo = "arturo.gomez@genelab.com";
+                userModel3.Activo = "Si";
+                userModel3.Rol = "Facturacion";
+
+                listUsers.Add(userModel1);
+                listUsers.Add(userModel2);
+                listUsers.Add(userModel3);
+
+
+                var data = new RespuestaAPI(listUsers);
+
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+
+                return Ok(ex);
+            }
+        }
+    }
+}
