cbbe7859022ebc20df3ba0c3892d7f397290998b
AlfredRdz <71676643+AlfredRdz@users.noreply.github.com>
2021-01-20 10:53:15 -0600

    Update RequestModel.cs
    integracion version alfredo

--- Genelab.API/Models/RequestModel.cs
+++ Genelab.API/Models/RequestModel.cs
@@ -7,21 +7,17 @@ namespace Genelab.API.Models
 {
     public class RequestModel
     {
-        public string Nombre { get; set; }
-        public string ApellidoPaterno { get; set; }
-        public string ApellidoMaterno { get; set; }
-        public string Titular { get; set; }
-        public string Parentesco { get; set; }
+        public string Id { get; set; }
+
+        public string Cliente { get; set; }
+
+        public string Paciente { get; set; }
+
+        public string Fecha { get; set; }
+
         public string Estudio { get; set; }
-        public string CP { get; set; }
-        public string Delegacion { get; set; }
-        public string Colonia { get; set; }
-        public string Calle { get; set; }
-        public string EmpresaFiscal { get; set; }
-        public string EmpresaFiscalCP { get; set; }
-        public string EmpresaFiscalDelegacion { get; set; }
-        public string EmpresaFiscalColonia { get; set; }
-        public string EmpresaFiscalCalle { get; set; }
+
+        public string Estatus { get; set; }
 
     }
 }
